df065002c468190b837dbda7c1be67fbe5e833b2
Alla781 <agingdoghealth@gmail.com>
2025-11-03 22:45:20 +0300

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,24 +1,6 @@
-export default function(eleventyConfig) {
-  // Passthrough
-  eleventyConfig.addPassthroughCopy({ "css": "css" });
-  eleventyConfig.addPassthroughCopy({ "images": "images" });
-  eleventyConfig.addPassthroughCopy({ "admin": "admin" });
-
-  // Watch targets
-  eleventyConfig.addWatchTarget("css/");
-
-  // Collections
-  eleventyConfig.addCollection("articles", (collectionApi) =>
-    collectionApi.getFilteredByGlob("./articles/*/.md").sort((a,b)=> (b.date - a.date))
-  );
-  eleventyConfig.addCollection("dogs", (collectionApi) =>
-    collectionApi.getFilteredByGlob("./dogs/*/.md").sort((a,b)=> a.data.title.localeCompare(b.data.title))
-  );
-
-  // Filters
-  eleventyConfig.addFilter("iso", date => new Date(date).toISOString());
-  eleventyConfig.addFilter("date", date => new Date(date).toDateString());
-  eleventyConfig.addFilter("url", (s) => (s || "").replace(/\/$/, ""));
+module.exports = function (eleventyConfig) {
+  // Copy /assets straight to /_site/assets
+  eleventyConfig.addPassthroughCopy({ "assets": "assets" });
 
   return {
     dir: {
@@ -27,7 +9,6 @@ export default function(eleventyConfig) {
       data: "_data",
       output: "_site"
     },
-    markdownTemplateEngine: "njk",
-    htmlTemplateEngine: "njk"
+    templateFormats: ["njk", "md", "html", "11ty.js"]
   };
-}
+};
