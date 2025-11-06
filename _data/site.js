78fc95c0e6012e3abb9108b065bfc76fb69772a6
Alla781 <agingdoghealth@gmail.com>
2025-11-06 10:00:24 +0300

    Create site.js

---
+++ _data/site.js
@@ -0,0 +1,7 @@
+// _data/site.js
+module.exports = {
+  name: "Aging Dog Health",
+  url: "https://agingdoghealth.com",
+  description: "Warm, expert guidance for caring owners of senior dogs.",
+  year: new Date().getFullYear()   // auto-updating footer year
+};
